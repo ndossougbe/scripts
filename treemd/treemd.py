ca94d41c1f2ebffead1456b4b04c2d59e253ba04
ndossougbe <nicolas.dossougbete@gmail.com>
2014-02-01 22:16:00 +0100

    treemd: encoding fix, added title to the html output

--- treemd/treemd.py
+++ treemd/treemd.py
@@ -7,7 +7,7 @@ import markdown
 import argparse
 
 
-def _applyStyle(name, isTerminal):
+def _apply_style(name, isTerminal):
 	name = name.replace('[','\[') # Avoid names with square brackets to be parsed to links
 	if isTerminal:
 		return name
@@ -15,34 +15,44 @@ def _applyStyle(name, isTerminal):
 		return '**{}**'.format(name)
 
 
+def _is_folder_to_ignore(foldername):
+	''' Returns false for hidden files (starting with '.') '''
+	return not foldername or foldername[0] == '.'
+
+
 def build_markdown(root):
 	output = '\n'
 	rootDepth = len(os.path.dirname(root).split(os.sep))
 
 	for path, dirs, files in os.walk(root):
-		dirs.sort() # the next step of walk follows the order of dirs
+		# prepare the next step
+		# print(dirs)
+		dirs[:] = [d for d in dirs if not _is_folder_to_ignore(d)]
+		dirs.sort()
 
 		head, tail = os.path.split(path)
-		
+
+		if _is_folder_to_ignore(tail): continue  # top level directories are not affected by the 'dirs' modificaiton 
+
 		padding = '\t' * (len(head.split(os.sep)) - rootDepth)
-		name = _applyStyle(tail, len(dirs) is 0)
+		name = _apply_style(tail, len(dirs) is 0)
 
 		output += '{}- {}\n'.format(padding, name)
 
 	return output
 
 
-def build_html(mdString, stylesheet, htmlTemplate):
+def build_html(mdString, args):
 	template, style = '{}{}',''
 
-	with open(htmlTemplate,'r', encoding='utf-8') as htmlFile:
+	with open(args.htmlTemplate,'r', encoding='utf-8') as htmlFile:
 		template = htmlFile.read()
 
-	if stylesheet:
-		with open(stylesheet, 'r', encoding='utf-8') as cssFile:
+	if args.stylesheet:
+		with open(args.stylesheet, 'r', encoding='utf-8') as cssFile:
 			style = cssFile.read()
 
-	return template.format(style , markdown.markdown(mdString, output_format='html5'))
+	return template.format(css=style, tree=markdown.markdown(mdString, output_format='html5'), root=args.dir)
 
 
 def _main(args):
@@ -54,7 +64,7 @@ def _main(args):
 
 	if args.html_output_enabled:
 		with open(os.path.join(args.output_dir, 'tree.html'),'w', encoding='utf-8') as html_output:
-			html_output.write(build_html(markdown, args.stylesheet, args.htmlTemplate))
+			html_output.write(build_html(markdown, args))
 
 
 if __name__ == '__main__':
@@ -71,7 +81,7 @@ if __name__ == '__main__':
 	parser.add_argument('--no-html', action='store_false', dest='html_output_enabled',
 						help='Disable HTML output.')
 	parser.add_argument('--html-template', dest='htmlTemplate', default=os.path.join(scriptDir, 'default.html'),
-						help='HTML skeleton. Requires two "{}" placeholders for the generated css and the html')
+						help='HTML skeleton. Requires "{css}", "{tree}" and "{root}" placeholders')
 	parser.add_argument('--css', dest='stylesheet', default=os.path.join(scriptDir, 'default.css'),
 						help='CSS stylesheet to use for the HTML output. Will be embedded in it.')
 
